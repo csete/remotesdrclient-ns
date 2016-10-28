05d95125337740c447ec806b32a944717f453719
Alexandru Csete <oz9aec@gmail.com>
2016-10-29 00:18:49 +0200

    Indent and format meter

--- gui/meter.cpp
+++ gui/meter.cpp
@@ -4,8 +4,8 @@
 //  This class creates and draws an S meter widget
 //
 // History:
-//	2013-10-02  Initial creation MSW
-//	2013-10-13  Added Squelch threshold marker
+//  2013-10-02  Initial creation MSW
+//  2013-10-13  Added Squelch threshold marker
 //////////////////////////////////////////////////////////////////////
 
 //==========================================================================================
@@ -16,11 +16,11 @@
 //permitted provided that the following conditions are met:
 //
 //   1. Redistributions of source code must retain the above copyright notice, this list of
-//	  conditions and the following disclaimer.
+//    conditions and the following disclaimer.
 //
 //   2. Redistributions in binary form must reproduce the above copyright notice, this list
-//	  of conditions and the following disclaimer in the documentation and/or other materials
-//	  provided with the distribution.
+//    of conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
 //
 //THIS SOFTWARE IS PROVIDED BY Moe Wheatley ``AS IS'' AND ANY EXPRESS OR IMPLIED
 //WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -40,26 +40,19 @@
 #include <stdlib.h>
 #include <QDebug>
 
-//////////////////////////////////////////////////////////////////////
-// Local defines
-//////////////////////////////////////////////////////////////////////
-//ratio to total control width or height
-#define CTRL_MARGIN 0.07		//left/right margin
-#define CTRL_MAJOR_START 0.4	//top of major tic line
-#define CTRL_MINOR_START 0.5	//top of minor tic line
-#define CTRL_XAXIS_HEGHT 0.6	//vert position of horizontal axis
-#define CTRL_NEEDLE_TOP 0.5		//vert position of top of needle triangle
+// ratio to total control width or height
+#define CTRL_MARGIN 0.07        // left/right margin
+#define CTRL_MAJOR_START 0.4    // top of major tic line
+#define CTRL_MINOR_START 0.5    // top of minor tic line
+#define CTRL_XAXIS_HEGHT 0.6    // vert position of horizontal axis
+#define CTRL_NEEDLE_TOP 0.5     // vert position of top of needle triangle
 
-#define MIN_DBM -121.0		//S1
-#define MAX_DBM -13.0		//S9+60
+#define MIN_DBM -121.0      // S1
+#define MAX_DBM -13.0       // S9+60
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
-CMeter::CMeter(QWidget *parent) :
-	QFrame(parent)
+CMeter::CMeter(QWidget * parent) : QFrame(parent)
 {
-	m_Overload = false;
+    m_Overload = false;
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     //setFocusPolicy(Qt::StrongFocus);
     //setAttribute(Qt::WA_PaintOnScreen,false);
@@ -68,21 +61,19 @@ CMeter::CMeter(QWidget *parent) :
     //setAttribute(Qt::WA_NoSystemBackground, true);
     //setMouseTracking(true);
 
-	m_Pixmap = QPixmap(0,0);
-	m_OverlayPixmap = QPixmap(0,0);
-	m_Size = QSize(0,0);
-	m_Slevel = 0;
-	m_dBm = -120;
-	m_SquelchPos = 0;
+    m_Pixmap = QPixmap(0,0);
+    m_OverlayPixmap = QPixmap(0, 0);
+    m_Size = QSize(0,0);
+    m_Slevel = 0;
+    m_dBm = -120;
+    m_SquelchPos = 0;
 }
 
 CMeter::~CMeter()
 {
 }
 
-//////////////////////////////////////////////////////////////////////
 // Sizing interface
-//////////////////////////////////////////////////////////////////////
 QSize CMeter::minimumSizeHint() const
 {
     return QSize(35, 10);
@@ -90,32 +81,28 @@ QSize CMeter::minimumSizeHint() const
 
 QSize CMeter::sizeHint() const
 {
-	return QSize(100, 30);
+    return QSize(100, 30);
 }
 
-
-//////////////////////////////////////////////////////////////////////
-// Called when screen size changes so must recalculate bitmaps
-//////////////////////////////////////////////////////////////////////
 void CMeter::resizeEvent(QResizeEvent* )
 {
-	if(!size().isValid())
-		return;
-    if( m_Size != size() )
-	{	//if size changed, resize pixmaps to new screensize
-		m_Size = size();
-		m_OverlayPixmap = QPixmap(m_Size.width(), m_Size.height());
+    if (!size().isValid())
+        return;
+
+    if (m_Size != size())
+    {
+        // if size changed, resize pixmaps to new screensize
+        m_Size = size();
+        m_OverlayPixmap = QPixmap(m_Size.width(), m_Size.height());
         m_OverlayPixmap.fill(Qt::white);
-		m_Pixmap = QPixmap(m_Size.width(), m_Size.height());
+        m_Pixmap = QPixmap(m_Size.width(), m_Size.height());
         m_Pixmap.fill(Qt::white);
-	}
-	DrawOverlay();
-	draw();
+    }
+    DrawOverlay();
+    draw();
 }
 
-//////////////////////////////////////////////////////////////////////
 // Slot called to update meter Receiver level position
-//////////////////////////////////////////////////////////////////////
 void CMeter::SetdBmLevel(double dbm, bool Overload)
 {
     double m_dBm_d = m_dBm;
@@ -128,220 +115,195 @@ void CMeter::SetdBmLevel(double dbm, bool Overload)
     m_dBm = (int) m_dBm_d;
     m_Slevel = CalcPosFromdB(m_dBm_d);
     if (m_Overload != Overload)
-	{
-		m_Overload = Overload;
-		DrawOverlay();
-	}
-	draw();
+    {
+        m_Overload = Overload;
+        DrawOverlay();
+    }
+    draw();
 }
 
-
-//////////////////////////////////////////////////////////////////////
 // called to calculate meter position from a dB level
-//////////////////////////////////////////////////////////////////////
 int CMeter::CalcPosFromdB(double db)
 {
-qreal w = (qreal)m_Pixmap.width();
-	w = w - 2.0*CTRL_MARGIN*w;	//width of meter scale in pixels
-	if(db<MIN_DBM)
-		db = MIN_DBM;
-	if(db>MAX_DBM)
-		db = MAX_DBM;
-	if(db <= -73.0)
-	{
-		qreal div = w/14.0;
-		return (int)( (db/6.0 + 121.0/6.0)*div + .5);
-	}
-	else
-	{
-		qreal div = w/14.0;
-		return (int)( ( 8 + db/10.0 + 73.0/10.0 )*div + .5);
-	}
+    qreal w = (qreal)m_Pixmap.width();
+    w = w - 2.0*CTRL_MARGIN*w;  //width of meter scale in pixels
+
+    if (db < MIN_DBM)
+        db = MIN_DBM;
+    else if (db > MAX_DBM)
+        db = MAX_DBM;
+
+    if (db <= -73.0)
+    {
+        qreal div = w / 14.0;
+        return (int)((db/6.0 + 121.0 / 6.0) * div + .5);
+    }
+    else
+    {
+        qreal div = w / 14.0;
+        return (int)((8 + db / 10.0 + 73.0 / 10.0) * div + .5);
+    }
 }
 
-//////////////////////////////////////////////////////////////////////
 // called to set Squelch Threshold meter position from a dB level
-//////////////////////////////////////////////////////////////////////
 void CMeter::SetSquelchPos(double db)
 {
-	m_SquelchPos = CalcPosFromdB(db);
-	DrawOverlay();
-	draw();
+    m_SquelchPos = CalcPosFromdB(db);
+    DrawOverlay();
+    draw();
 }
 
-
-//////////////////////////////////////////////////////////////////////
 // Slot called to update transmit audio meter level position
-//////////////////////////////////////////////////////////////////////
 void CMeter::SetTxLevel(double db)
 {
-qreal w = (qreal)m_Pixmap.width();
-	w = w - 2.0*CTRL_MARGIN*w;	//width of meter scale in pixels
-	m_dBm = (int)db;
-	if(db<-60)
-		db = -60;
-	if(db>0)
-		db = 0;
-	qreal div = w/14.0;
-	m_Slevel = (int)(( db*14.0/60.0 + 14.0)*div + .5);
-	if(db > -3.0)
-	{
-		if(!m_Overload)
-		{
-			m_Overload = true;
-			DrawOverlay();
-		}
-	}
-	else
-	{
-		if(m_Overload)
-		{
-			m_Overload = false;
-			DrawOverlay();
-		}
-	}
-	draw();
+    qreal w = (qreal)m_Pixmap.width();
+    w = w - 2.0*CTRL_MARGIN*w;  //width of meter scale in pixels
+
+    m_dBm = (int)db;
+    if (db < -60)
+        db = -60;
+    else if (db >0)
+        db = 0;
+
+    qreal div = w / 14.0;
+    m_Slevel = (int)((db * 14.0 / 60.0 + 14.0) * div + .5);
+    if (db > -3.0)
+    {
+        if (!m_Overload)
+        {
+            m_Overload = true;
+            DrawOverlay();
+        }
+    }
+    else
+    {
+        if (m_Overload)
+        {
+            m_Overload = false;
+            DrawOverlay();
+        }
+    }
+    draw();
 }
 
-//////////////////////////////////////////////////////////////////////
 // Called by Qt when screen needs to be redrawn
-//////////////////////////////////////////////////////////////////////
 void CMeter::paintEvent(QPaintEvent *)
 {
-	QPainter painter(this);
-	painter.drawPixmap(0,0,m_Pixmap);
-	return;
+    QPainter painter(this);
+    painter.drawPixmap(0,0,m_Pixmap);
+    return;
 }
 
-//////////////////////////////////////////////////////////////////////
 // Called to update meter data display on the screen
-//////////////////////////////////////////////////////////////////////
 void CMeter::draw()
 {
-int w;
-int h;
-	if(m_Pixmap.isNull())
-		return;
-	//get/draw the 2D spectrum
-	w = m_Pixmap.width();
-	h = m_Pixmap.height();
-	//first copy into 2Dbitmap the overlay bitmap.
-	m_Pixmap = m_OverlayPixmap.copy(0,0,w,h);
-	QPainter painter(&m_Pixmap);
+    int w;
+    int h;
 
-	//DrawCurrent position indicator
-	qreal hline = (qreal)h*CTRL_XAXIS_HEGHT;
-	qreal marg = (qreal)w*CTRL_MARGIN;
-	qreal ht = (qreal)h*CTRL_NEEDLE_TOP;
-	qreal x = marg + m_Slevel;
-	QPoint pts[3];
-	pts[0].setX(x); pts[0].setY(ht);
-	pts[1].setX(x-6); pts[1].setY(hline+6);
-	pts[2].setX(x+6); pts[2].setY(hline+6);
+    if (m_Pixmap.isNull())
+        return;
 
+    w = m_Pixmap.width();
+    h = m_Pixmap.height();
 
-	if(m_Overload)
-		painter.setBrush(QBrush(Qt::red));
-	else
-		painter.setBrush(QBrush(Qt::yellow));
-	painter.setOpacity(1.0);
-	painter.drawPolygon(pts,3);
+    // first copy into 2Dbitmap the overlay bitmap.
+    m_Pixmap = m_OverlayPixmap.copy(0, 0, w, h);
+    QPainter painter(&m_Pixmap);
 
-	//create Font to use for scales
+    //DrawCurrent position indicator
+    qreal hline = (qreal) h * CTRL_XAXIS_HEGHT;
+    qreal marg = (qreal) w * CTRL_MARGIN;
+    qreal ht = (qreal) h * CTRL_NEEDLE_TOP;
+    qreal x = marg + m_Slevel;
+    QPoint pts[3];
+    pts[0].setX(x); pts[0].setY(ht);
+    pts[1].setX(x-6); pts[1].setY(hline+6);
+    pts[2].setX(x+6); pts[2].setY(hline+6);
+
+    if (m_Overload)
+        painter.setBrush(QBrush(Qt::red));
+    else
+        painter.setBrush(QBrush(Qt::yellow));
+
+    painter.setOpacity(1.0);
+    painter.drawPolygon(pts,3);
+
+    // create Font to use for scales
     QFont Font;
-	int y = (h)/3;
-	Font.setPixelSize(y);
-	Font.setWeight(QFont::Normal);
-	painter.setFont(Font);
+    int y = h / 3;
+    Font.setPixelSize(y);
+    Font.setWeight(QFont::Normal);
+    painter.setFont(Font);
 
     painter.setPen(QColor(0xFF1F1D1D));
-	painter.setOpacity(1.0);
-	m_Str.setNum(m_dBm);
-	painter.drawText(marg, h-1, m_Str+" dBm" );
-	update();
+    painter.setOpacity(1.0);
+    m_Str.setNum(m_dBm);
+    painter.drawText(marg, h - 1, m_Str + " dBm");
+    update();
 }
 
-//////////////////////////////////////////////////////////////////////
 // Called to draw an overlay bitmap containing graphics that
 // does not need to be recreated every data update.
-//////////////////////////////////////////////////////////////////////
 void CMeter::DrawOverlay()
 {
-	if(m_OverlayPixmap.isNull())
-		return;
-	int w = m_OverlayPixmap.width();
-	int h = m_OverlayPixmap.height();
-	int x,y;
-	QRect rect;
-	QPainter painter(&m_OverlayPixmap);
+    int w = m_OverlayPixmap.width();
+    int h = m_OverlayPixmap.height();
+    int x, y;
+    QRect rect;
+    QPainter painter(&m_OverlayPixmap);
 
-    // fill background with gradient
-#if 0
-    QLinearGradient gradient(0, 0, 0 ,h);
-	if(m_Overload)
-	{
-		gradient.setColorAt(1, Qt::white);
-		gradient.setColorAt(0, Qt::red);
-	}
-	else
-	{
-		gradient.setColorAt(1, Qt::cyan);
-		gradient.setColorAt(0, Qt::blue);
-	}
-    painter.setBrush(gradient);
-#endif
+    if (m_OverlayPixmap.isNull())
+        return;
+
     painter.setPen(Qt::white);      // needed to avoid black line on left and upper sides
     if (m_Overload)
         painter.setBrush(QBrush(Qt::darkRed, Qt::SolidPattern));
     else
         painter.setBrush(QBrush(Qt::white, Qt::SolidPattern));
-	painter.drawRect(0, 0, w, h);
+    painter.drawRect(0, 0, w, h);
 
-	//Draw scale lines
-	qreal marg = (qreal)w*CTRL_MARGIN;
-	qreal hline = (qreal)h*CTRL_XAXIS_HEGHT;
-	qreal majstart = (qreal)h*CTRL_MAJOR_START;
-	qreal minstart = (qreal)h*CTRL_MINOR_START;
-	qreal hstop = (qreal)w-marg;
+    // scale lines
+    qreal marg = (qreal) w * CTRL_MARGIN;
+    qreal hline = (qreal) h * CTRL_XAXIS_HEGHT;
+    qreal majstart = (qreal) h * CTRL_MAJOR_START;
+    qreal minstart = (qreal) h * CTRL_MINOR_START;
+    qreal hstop = (qreal) w - marg;
     painter.setPen(QPen(QColor(0xFF1F1D1D), 1, Qt::SolidLine));
-    painter.drawLine(QLineF( marg, hline, hstop, hline));
-	qreal xpos = marg;
-	for(x=0; x<15; x++)
-	{
-		if(x&1)	//minor tics
-			painter.drawLine( QLineF(xpos, minstart, xpos, hline) );
-		else
-			painter.drawLine( QLineF(xpos, majstart, xpos, hline) );
-		xpos += (hstop-marg)/14.0;
-	}
+    painter.drawLine(QLineF(marg, hline, hstop, hline));
+    qreal xpos = marg;
+    for (x = 0; x < 15; x++)
+    {
+        if (x & 1)  // minor tics
+            painter.drawLine(QLineF(xpos, minstart, xpos, hline));
+        else
+            painter.drawLine(QLineF(xpos, majstart, xpos, hline));
+        xpos += (hstop - marg) / 14.0;
+    }
 
-
-	//draw scale text
-	//create Font to use for scales
+    // draw scale text
+    // create Font to use for scales
     QFont Font;
-//	QFontMetrics metrics(Font);
-	y = h/4;
-	Font.setPixelSize(y);
-	Font.setWeight(QFont::Normal);
-	painter.setFont(Font);
-	int rwidth = (int)((hstop-marg)/7.0);
-	m_Str = "+60";
-	rect.setRect(marg/2, 0, rwidth, majstart);
-	for(x=1; x<=9; x+=2)
-	{
-		m_Str.setNum(x);
-		painter.drawText(rect, Qt::AlignHCenter|Qt::AlignVCenter, m_Str);
-		rect.translate( rwidth,0);
-	}
+    y = h/4;
+    Font.setPixelSize(y);
+    Font.setWeight(QFont::Normal);
+    painter.setFont(Font);
+    int rwidth = (int)((hstop - marg) / 7.0);
+    m_Str = "+60";
+    rect.setRect(marg/2, 0, rwidth, majstart);
+    for (x = 1; x <= 9; x += 2)
+    {
+        m_Str.setNum(x);
+        painter.drawText(rect, Qt::AlignHCenter|Qt::AlignVCenter, m_Str);
+        rect.translate(rwidth, 0);
+    }
     painter.setPen(QPen(Qt::darkRed, 1, Qt::SolidLine));
-	for(x=20; x<=60; x+=20)
-	{
-		m_Str = "+" + m_Str.setNum(x);
-		painter.drawText(rect, Qt::AlignHCenter|Qt::AlignVCenter, m_Str);
-		rect.translate( rwidth,0);
-	}
-	//Draw Squelch Threshold Position thingy
+    for (x = 20; x <= 60; x += 20)
+    {
+        m_Str = "+" + m_Str.setNum(x);
+        painter.drawText(rect, Qt::AlignHCenter|Qt::AlignVCenter, m_Str);
+        rect.translate(rwidth, 0);
+    }
+    // Draw Squelch Threshold Position thingy
     painter.setPen(QPen(Qt::darkRed, 2, Qt::SolidLine));
-	painter.drawEllipse( marg + m_SquelchPos-1, majstart, 2, 2 );
+    painter.drawEllipse(marg + m_SquelchPos-1, majstart, 2, 2);
 }
-
--- gui/meter.h
+++ gui/meter.h
@@ -2,7 +2,7 @@
 // meter.h: interface for the CMeter class.
 //
 // History:
-//	2013-10-02  Initial creation MSW
+//  2013-10-02  Initial creation MSW
 /////////////////////////////////////////////////////////////////////
 #ifndef METER_H
 #define METER_H
@@ -16,38 +16,35 @@ class CMeter : public QFrame
 {
     Q_OBJECT
 public:
-	explicit CMeter(QWidget *parent = 0);
-	~CMeter();
+    explicit CMeter(QWidget *parent = 0);
+    ~CMeter();
 
-	QSize minimumSizeHint() const;
-	QSize sizeHint() const;
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
 
-	void draw();		//call to draw new fft data onto screen plot
-	void UpdateOverlay(){DrawOverlay();}
-	void SetSquelchPos(double db);		//{ m_SquelchPos = CalcPosFromdB(db);}
-
-signals:
+    void draw();
+    void UpdateOverlay() { DrawOverlay(); }
+    void SetSquelchPos(double db);
 
 public slots:
-	void SetdBmLevel(double dbm, bool Overload);
-	void SetTxLevel(double db);
+    void SetdBmLevel(double dbm, bool Overload);
+    void SetTxLevel(double db);
 
 protected:
-		//re-implemented widget event handlers
-	void paintEvent(QPaintEvent *event);
-	void resizeEvent(QResizeEvent* event);
+    void paintEvent(QPaintEvent *event);
+    void resizeEvent(QResizeEvent* event);
 
 private:
-	void DrawOverlay();
-	int CalcPosFromdB(double db);
-	QPixmap m_Pixmap;
-	QPixmap m_OverlayPixmap;
-	QSize m_Size;
-	QString m_Str;
-	bool m_Overload;
-	int m_Slevel;
-	int m_SquelchPos;
-	int m_dBm;
+    void DrawOverlay();
+    int CalcPosFromdB(double db);
+    QPixmap m_Pixmap;
+    QPixmap m_OverlayPixmap;
+    QSize m_Size;
+    QString m_Str;
+    bool m_Overload;
+    int m_Slevel;
+    int m_SquelchPos;
+    int m_dBm;
 };
 
 #endif // METER_H
