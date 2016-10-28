37bab61da04b56dafa7e83d29387a1039976067e
Alexandru Csete <oz9aec@gmail.com>
2016-10-29 00:45:26 +0200

    Bump version

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -76,7 +76,7 @@
 #include <QSpinBox>
 #include <QSettings>
 
-#define PROGRAM_TITLE_VERSION "RemoteSdrClient 1.11-ns10"
+#define PROGRAM_TITLE_VERSION "RemoteSdrClient 1.11-ns11"
 
 
 #define DOWNCONVERTER_TRANSITION_FREQ 56000000	//frequency where transition between direct and downconverter mode occurs
